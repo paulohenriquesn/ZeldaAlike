b3566dab37d36e926d3c65187cbc5d008bf1c13c
mscurie <gabrielle.clima23@gmail.com>
2019-03-10 20:41:24 -0300

    vai logo carai

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class  Player : MonoBehaviour {
+ 
+    // Start is called before the first frame update
+    public float speed;
+
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(); 
+    }
+    void Movement() {
+     
+    if(Input.GetKey(KeyCode.W))
+    { transform.Translate(0,speed*Time.deltaTime,0);
+    }
+      else if(Input.GetKey(KeyCode.S))
+    {
+        transform.Translate(0,-speed*Time.deltaTime,0);
+        }
+     else if (Input.GetKey(KeyCode.A))
+     {
+         transform.Translate(-speed*Time.deltaTime,0,0);
+         }
+     else if(Input.GetKey(KeyCode.D))
+     {
+            transform.Translate(speed*Time.deltaTime,0,0);
+         }
+    }
+}
