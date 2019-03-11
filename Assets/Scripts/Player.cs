6e9d7e7bdf4e9d54a160ac94e248f661a8d79452
mscurie <gabrielle.clima23@gmail.com>
2019-03-11 12:37:01 -0300

    subindo o que faltou

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,12 +16,31 @@ public class  Player : MonoBehaviour {
     void Start()
     { 
         Anim = GetComponent<Animator>();
+        CurrentHealth = MaxHealth;
+        GetHealth();
+    }
+    void GetHealth() {
+        for(int i= 0; i <= hearts.Length -1; i++)
+        {
+      hearts[i].gameObject.SetActive(false);
+        }
+        for(int i = 0; i <= CurrentHealth -1; i++) {
+            hearts[i].gameObject.SetActive(true);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement(); 
+        if(Input.GetKeyDown(KeyCode.P))
+        CurrentHealth--;
+        GetHealth();
+        if (Input.GetKeyDown(KeyCode.L))
+        CurrentHealth++;
+        if(CurrentHealth > MaxHealth)
+        CurrentHealth = MaxHealth;
     }
     void Movement() {
      
@@ -38,6 +57,7 @@ public class  Player : MonoBehaviour {
      {
          transform.Translate(-speed*Time.deltaTime,0,0);
          Anim.SetInteger("dir", 2);
+         
          }
      else if(Input.GetKey(KeyCode.D))
      {
