ec6331b6aca5620ccd2d0ed9f5d6ebc6c100fe55
mscurie <gabrielle.clima23@gmail.com>
2019-03-10 22:47:45 -0300

    atualizações do código

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,11 @@ public class  Player : MonoBehaviour {
  
     // Start is called before the first frame update
     public float speed;
+    Animator anim;
 
     void Start()
     { 
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,19 +22,23 @@ public class  Player : MonoBehaviour {
     void Movement() {
      
     if(Input.GetKey(KeyCode.W))
-    { transform.Translate(0,speed*Time.deltaTime,0);
+    { transform.Translate(0,speed*Time.deltaTime,0); 
+    anim.setInteger("dir", 0);
     }
       else if(Input.GetKey(KeyCode.S))
     {
         transform.Translate(0,-speed*Time.deltaTime,0);
+        anim.setInteger("dir", 1);
         }
      else if (Input.GetKey(KeyCode.A))
      {
          transform.Translate(-speed*Time.deltaTime,0,0);
+         anim.setInteger("dir", 2);
          }
      else if(Input.GetKey(KeyCode.D))
      {
             transform.Translate(speed*Time.deltaTime,0,0);
+            anim.setInteger("dir", 3);
          }
     }
 }
