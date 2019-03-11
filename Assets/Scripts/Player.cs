e0cac73db2e783429897ec1a207a5b59ba6f3d93
mscurie <gabrielle.clima23@gmail.com>
2019-03-10 23:00:16 -0300

    animação do personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,11 @@ public class  Player : MonoBehaviour {
  
     // Start is called before the first frame update
     public float speed;
-    Animator anim;
+    Animator Anim;
 
     void Start()
     { 
-        anim = GetComponent<Animator>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,22 +23,22 @@ public class  Player : MonoBehaviour {
      
     if(Input.GetKey(KeyCode.W))
     { transform.Translate(0,speed*Time.deltaTime,0); 
-    anim.setInteger("dir", 0);
+    Anim.SetInteger("dir", 0);
     }
       else if(Input.GetKey(KeyCode.S))
     {
         transform.Translate(0,-speed*Time.deltaTime,0);
-        anim.setInteger("dir", 1);
+        Anim.SetInteger("dir", 1);
         }
      else if (Input.GetKey(KeyCode.A))
      {
          transform.Translate(-speed*Time.deltaTime,0,0);
-         anim.setInteger("dir", 2);
+         Anim.SetInteger("dir", 2);
          }
      else if(Input.GetKey(KeyCode.D))
      {
             transform.Translate(speed*Time.deltaTime,0,0);
-            anim.setInteger("dir", 3);
+            Anim.SetInteger("dir", 3);
          }
     }
 }
